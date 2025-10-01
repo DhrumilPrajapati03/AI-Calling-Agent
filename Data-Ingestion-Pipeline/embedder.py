26de5f91bb5fdf9df521876077ca44ad1896292e
DhrumilPrajapati03 <prajapatidhrumil2022@gmail.com>
2025-10-01 16:34:19 +0530

    Remove all but RAG-Pipeline from RAG-pipeline branch

--- Data-Ingestion-Pipeline/embedder.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.embeddings import HuggingFaceEmbeddings
-
-embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/distiluse-base-multilingual-cased-v1"
-)
-
-def embed_texts(texts):
-    return embedding_model.embed_documents(texts)
