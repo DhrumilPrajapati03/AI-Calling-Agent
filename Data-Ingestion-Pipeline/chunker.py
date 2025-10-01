2f4d5d5cc65e4d0870aac107385f64a55d62befa
DhrumilPrajapati03 <prajapatidhrumil2022@gmail.com>
2025-10-01 14:55:24 +0530

    Keep only agent-related code in Agent-pipeline branch

--- Data-Ingestion-Pipeline/chunker.py
+++
@@ -1,9 +0,0 @@
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-def chunk_text(text, chunk_size=800, overlap=100):
-    splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size,
-        chunk_overlap=overlap,
-        length_function=len,
-    )
-    return splitter.split_text(text)
